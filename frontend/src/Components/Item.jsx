3d79ca7464e7531aa5de88598bb4ded5ee10caeb
madhavirabbagungu@gmail.com <madhavirabbgundu@gmail.com>
2022-07-29 18:54:42 +0530

    complete homepage

---
+++ frontend/src/Components/Item.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+background-color:white;
+color:black;
+width:100%;
+`
